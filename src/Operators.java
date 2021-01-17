5ea25a054c5856a62bfb0abbad50bd2b789250fa
youngasung4 <youngasung4@gmail.com>
2021-01-17 12:16:29 +0900

    first commit

---
+++ src/Operators.java
@@ -0,0 +1,15 @@
+public class Operators {
+
+    public void calculator(){
+        System.out.println(1 + 2);
+        System.out.println(3 - 1);
+        System.out.println(5 * 4);
+        System.out.println(6 / 2);
+
+        //special case
+        System.out.println(5 / 2);
+        System.out.println(5 % 2);
+
+
+    }
+}
---
+++ src/Testmain.java
@@ -0,0 +1,14 @@
+public class Testmain {
+    public static void main(String[] args){
+        //System.out.println("hello");
+
+        // operators
+        Operators operators = new Operators();
+        operators.calculator();
+
+
+    }
+}
+
+
+
